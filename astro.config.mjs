72dd2fcaa36df4a49c2060d93890b6620f1e3977
josejooj <josejooj.dev@gmail.com>
2025-10-02 09:10:17 -0300

    refactor: server config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    server: {
+        port: 80,
+        host: true
+    },
     prefetch: {
         defaultStrategy: "viewport",
         prefetchAll: true
-    }
+    },
+    
 });
